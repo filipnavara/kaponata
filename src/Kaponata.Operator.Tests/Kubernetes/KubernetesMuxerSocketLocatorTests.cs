e52003595e0e79dad99ddb617eaf8373e7977375
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-01-19 17:37:37 +0100

    Add a KubernetesMuxerSocketLocator which connects to usbmuxd running in a Kubernetes pod.

---
+++ src/Kaponata.Operator.Tests/Kubernetes/KubernetesMuxerSocketLocatorTests.cs
@@ -0,0 +1,78 @@
+﻿// <copyright file="KubernetesMuxerSocketLocatorTests.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using k8s;
+using k8s.Models;
+using Kaponata.Operator.Kubernetes;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kaponata.Operator.Tests.Kubernetes
+{
+    /// <summary>
+    /// Tests the <see cref="KubernetesMuxerSocketLocator"/> class.
+    /// </summary>
+    public class KubernetesMuxerSocketLocatorTests
+    {
+        /// <summary>
+        /// The <see cref="KubernetesMuxerSocketLocator"/> constructor validates its arguments.
+        /// </summary>
+        [Fact]
+        public void Constructor_ValidatesArguments()
+        {
+            Assert.Throws<ArgumentNullException>("kubernetes", () => new KubernetesMuxerSocketLocator(null, new V1Pod(), NullLogger<KubernetesMuxerSocketLocator>.Instance, NullLoggerFactory.Instance));
+            Assert.Throws<ArgumentNullException>("pod", () => new KubernetesMuxerSocketLocator(Mock.Of<IKubernetes>(), null, NullLogger<KubernetesMuxerSocketLocator>.Instance, NullLoggerFactory.Instance));
+            Assert.Throws<ArgumentNullException>("logger", () => new KubernetesMuxerSocketLocator(Mock.Of<IKubernetes>(), new V1Pod(), null, NullLoggerFactory.Instance));
+            Assert.Throws<ArgumentNullException>("loggerFactory", () => new KubernetesMuxerSocketLocator(Mock.Of<IKubernetes>(), new V1Pod(), NullLogger<KubernetesMuxerSocketLocator>.Instance, null));
+        }
+
+        /// <summary>
+        /// <see cref="KubernetesMuxerSocketLocator.ConnectToMuxerAsync"/> uses Kubernetes port forwarding.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Task"/> which represents the asynchronous test.
+        /// </returns>
+        [Fact]
+        public async Task ConnectToMuxerAsync_UsesPortForwarding_Async()
+        {
+            var pod = new V1Pod()
+            {
+                Metadata = new V1ObjectMeta()
+                {
+                    NamespaceProperty = "my-namespace",
+                    Name = "usbmuxd-abcd",
+                },
+            };
+
+            var websocket = Mock.Of<WebSocket>();
+            var kubernetes = new Mock<IKubernetes>(MockBehavior.Strict);
+            kubernetes
+                .Setup(k => k.WebSocketNamespacedPodPortForwardAsync("usbmuxd-abcd", "my-namespace", new int[] { 27015 }, null, null, default))
+                .ReturnsAsync(websocket);
+
+            var locator = new KubernetesMuxerSocketLocator(kubernetes.Object, pod, NullLogger<KubernetesMuxerSocketLocator>.Instance, NullLoggerFactory.Instance);
+            using (var stream = await locator.ConnectToMuxerAsync(default))
+            {
+                var portForwardStream = Assert.IsType<PortForwardStream>(stream);
+                Assert.Same(websocket, portForwardStream.WebSocket);
+            }
+
+            kubernetes.Verify();
+        }
+
+        /// <summary>
+        /// <see cref="KubernetesMuxerSocketLocator.GetMuxerSocket"/> is not implemented.
+        /// </summary>
+        [Fact]
+        public void GetMuxerSocket_Throws()
+        {
+            var locator = new KubernetesMuxerSocketLocator(Mock.Of<IKubernetes>(), new V1Pod(), NullLogger<KubernetesMuxerSocketLocator>.Instance, NullLoggerFactory.Instance);
+            Assert.Throws<NotSupportedException>(() => locator.GetMuxerSocket());
+        }
+    }
+}
---
+++ src/Kaponata.Operator/Kubernetes/KubernetesMuxerSocketLocator.cs
@@ -0,0 +1,79 @@
+﻿// <copyright file="KubernetesMuxerSocketLocator.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using k8s;
+using k8s.Models;
+using Kaponata.iOS.Muxer;
+using Kaponata.Operator.Kubernetes;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kaponata.Operator.Kubernetes
+{
+    /// <summary>
+    /// An implementation of the <see cref="MuxerSocketLocator"/> which connects to usbmuxd running in a Kubernetes pod,
+    /// using port forwarding over the API server.
+    /// </summary>
+    public class KubernetesMuxerSocketLocator : MuxerSocketLocator
+    {
+        private readonly ILogger<KubernetesMuxerSocketLocator> logger;
+        private readonly ILoggerFactory loggerFactory;
+        private readonly IKubernetes kubernetes;
+        private readonly V1Pod pod;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KubernetesMuxerSocketLocator"/> class.
+        /// </summary>
+        /// <param name="kubernetes">
+        /// A connection to the Kubernetes cluster.
+        /// </param>
+        /// <param name="pod">
+        /// The pod in which usbmuxd is running.
+        /// </param>
+        /// <param name="logger">
+        /// A logger which is used when logging.
+        /// </param>
+        /// <param name="loggerFactory">
+        /// A logger factory which an be used to create new loggers.
+        /// </param>
+        public KubernetesMuxerSocketLocator(
+            IKubernetes kubernetes,
+            V1Pod pod,
+            ILogger<KubernetesMuxerSocketLocator> logger,
+            ILoggerFactory loggerFactory)
+        {
+            this.kubernetes = kubernetes ?? throw new ArgumentNullException(nameof(kubernetes));
+            this.pod = pod ?? throw new ArgumentNullException(nameof(pod));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+        }
+
+        /// <inheritdoc/>
+        public override async Task<Stream> ConnectToMuxerAsync(CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("Connecting to port {port} on pod {pod}", DefaultMuxerPort, this.pod.Metadata.Name);
+
+            var webSocket = await this.kubernetes.WebSocketNamespacedPodPortForwardAsync(
+                this.pod.Metadata.Name,
+                this.pod.Metadata.NamespaceProperty,
+                new int[] { DefaultMuxerPort },
+                cancellationToken: cancellationToken).ConfigureAwait(false);
+
+            this.logger.LogInformation("Connected to port {port} on pod {pod}", DefaultMuxerPort, this.pod.Metadata.Name);
+            return new PortForwardStream(webSocket, this.loggerFactory.CreateLogger<PortForwardStream>());
+
+        }
+
+        /// <inheritdoc/>
+        public override (Socket, EndPoint) GetMuxerSocket()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
