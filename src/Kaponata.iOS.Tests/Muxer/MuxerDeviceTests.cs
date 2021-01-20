876e7651e9098674596c95108863f14dddc87106
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-01-20 23:42:59 +0100

    Fix MuxerDevice code coverage

---
+++ src/Kaponata.iOS.Tests/Muxer/MuxerDeviceTests.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="MuxerDeviceTests.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Kaponata.iOS.Muxer;
+using Xunit;
+
+namespace Kaponata.iOS.Tests.Muxer
+{
+    /// <summary>
+    /// Tests the <see cref="MuxerDevice"/> class.
+    /// </summary>
+    public class MuxerDeviceTests
+    {
+        /// <summary>
+        /// <see cref="MuxerDevice.ToString"/> returns the device UDID.
+        /// </summary>
+        [Fact]
+        public void ToString_ReturnsUdid()
+        {
+            var device = new MuxerDevice()
+            {
+                Udid = "abc",
+            };
+
+            Assert.Equal("abc", device.ToString());
+        }
+    }
+}
