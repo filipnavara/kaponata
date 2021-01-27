942a6692681e6974da37a774f270d156d7dc553d
Bart Saint Germain <bart.saintgermain@quamotion.mobi>
2021-01-27 17:44:48 +0100

    Add ScrCpyController

---
+++ src/Kaponata.Android.Tests/ScrCpy/ScrCpyControllerTests.cs
@@ -0,0 +1,84 @@
+﻿// <copyright file="ScrCpyControllerTests.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Kaponata.Android.ScrCpy;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kaponata.Android.Tests.ScrCpy
+{
+    /// <summary>
+    /// Tests the <see cref="ScrCpyController"/> class.
+    /// </summary>
+    public class ScrCpyControllerTests
+    {
+        /// <summary>
+        /// The <see cref="ScrCpyController"/> constructor validate the argments being passed.
+        /// </summary>
+        [Fact]
+        public void Constructor_ValidatesArguments()
+        {
+            using var stream = new MemoryStream();
+            Assert.Throws<ArgumentNullException>(() => new ScrCpyController(new DeviceInfo() { }, stream, null));
+            Assert.Throws<ArgumentNullException>(() => new ScrCpyController(new DeviceInfo() { }, null, NullLogger<ScrCpyController>.Instance));
+        }
+
+        /// <summary>
+        /// The <see cref="ScrCpyClient"/> constructor initializes the properties.
+        /// </summary>
+        [Fact]
+        public void Constructor_Properties()
+        {
+            using var stream = new MemoryStream();
+            var logger = new NullLogger<ScrCpyController>();
+            var controller = new ScrCpyController(new DeviceInfo() { DeviceName = "123" }, stream, logger);
+
+            Assert.Equal("123", controller.DeviceInfo.DeviceName);
+        }
+
+        /// <summary>
+        /// The <see cref="ScrCpyController.SendControlMessageAsync(IControlMessage, System.Threading.CancellationToken)"/> throws when invalid device info is passed.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Task"/> which represents the asynchronous test.
+        /// </returns>
+        [Fact]
+        public async Task SendControlMessage_ValidatesArgument_Async()
+        {
+            using var stream = new MemoryStream();
+            var controller = new ScrCpyController(new DeviceInfo() { DeviceName = "123" }, stream, NullLogger<ScrCpyController>.Instance);
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await controller.SendControlMessageAsync(null, default));
+        }
+
+        /// <summary>
+        /// The <see cref="ScrCpyController.SendControlMessageAsync(IControlMessage, System.Threading.CancellationToken)"/> sends the message.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Task"/> which represents the asynchronous test.
+        /// </returns>
+        [Fact]
+        public async Task SendControlMessage_SendsMessage_Async()
+        {
+            using var stream = new MemoryStream();
+            var controller = new ScrCpyController(new DeviceInfo() { DeviceName = "123" }, stream, NullLogger<ScrCpyController>.Instance);
+
+            var messageMock = new Mock<IControlMessage>();
+            messageMock.SetupGet(m => m.BinarySize).Returns(100);
+            messageMock.Setup(m => m.Write(It.IsAny<Memory<byte>>())).Callback<Memory<byte>>(s => new byte[] { 1, 2, 3 }.CopyTo(s));
+
+            await controller.SendControlMessageAsync(messageMock.Object, default).ConfigureAwait(false);
+
+            stream.Position = 0;
+            Assert.Equal(100, stream.Length);
+            Assert.Equal(1, stream.ReadByte());
+            Assert.Equal(2, stream.ReadByte());
+            Assert.Equal(3, stream.ReadByte());
+        }
+    }
+}
---
+++ src/Kaponata.Android/ScrCpy/ScrCpyController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kaponata.Android.ScrCpy
+{
+    /// <summary>
+    /// Class used to send messages to the scrcpy server.
+    /// </summary>
+    public class ScrCpyController : IDisposable
+    {
+        /// <summary>
+        /// The underlying stream to which control messages are being written.
+        /// </summary>
+        private readonly Stream stream;
+        private readonly ILogger<ScrCpyController> logger;
+        private readonly MemoryPool<byte> memoryPool = MemoryPool<byte>.Shared;
+        private bool disposed = false;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScrCpyController"/> class.
+        /// </summary>
+        /// <param name="deviceInfo">
+        /// The scrcpy device info.
+        /// </param>
+        /// <param name="stream">
+        /// The stream to which scrcpy control messages can be transmitted.
+        /// </param>
+        /// <param name="logger">
+        /// A logger to use when logging diagnostic data.
+        /// </param>
+        public ScrCpyController(DeviceInfo deviceInfo, Stream stream, ILogger<ScrCpyController> logger)
+        {
+            this.stream = stream ?? throw new ArgumentNullException(nameof(stream));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            this.DeviceInfo = deviceInfo;
+        }
+
+        /// <summary>
+        /// Gets the scrcpy device info.
+        /// </summary>
+        public DeviceInfo DeviceInfo
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this controller is disposed.
+        /// </summary>
+        public bool Disposed
+        {
+            get
+            {
+                return this.disposed;
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.stream.Dispose();
+            this.disposed = true;
+        }
+
+        /// <summary>
+        /// Sends a scrcpy control message.
+        /// </summary>
+        /// <param name="message">
+        /// The message to be transmitted.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// A <see cref="CancellationToken"/> which can be used to cancel the asynchronous operation.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the asynchronous operation.
+        /// </returns>
+        public virtual async Task SendControlMessageAsync(IControlMessage message, CancellationToken cancellationToken)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            // Write the message in a buffer
+            var size = message.BinarySize;
+            using var data = this.memoryPool.Rent(size);
+            message.Write(data.Memory.Slice(0, size));
+
+            // Send the packet
+            await this.stream.WriteAsync(data.Memory.Slice(0, size), cancellationToken).ConfigureAwait(false);
+            await this.stream.FlushAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
