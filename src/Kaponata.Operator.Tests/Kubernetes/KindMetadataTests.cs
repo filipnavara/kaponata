eb99f0dfdee1d6866768c4532e9da4ef37c31813
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-01-28 11:39:03 +0100

    KubernetesClient: Move generic code to KubernetsClient.NamespacedObject

---
+++ src/Kaponata.Operator.Tests/Kubernetes/KindMetadataTests.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="KindMetadataTests.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Kaponata.Operator.Kubernetes;
+using System;
+using Xunit;
+
+namespace Kaponata.Operator.Tests.Kubernetes
+{
+    /// <summary>
+    /// Tests the <see cref="KindMetadata"/> class.
+    /// </summary>
+    public class KindMetadataTests
+    {
+        /// <summary>
+        /// The <see cref="KindMetadata"/> validates its arguments.
+        /// </summary>
+        public void Constructor_ValidatesArguments()
+        {
+            Assert.Throws<ArgumentNullException>("group", () => new KindMetadata("group", "version", "plural"));
+            Assert.Throws<ArgumentNullException>("version", () => new KindMetadata("group", null, "plural"));
+            Assert.Throws<ArgumentNullException>("plural", () => new KindMetadata("group", "version", null));
+        }
+
+        /// <summary>
+        /// The <see cref="KindMetadata"/> intializes the object properties.
+        /// </summary>
+        public void Constructor_SetsProperties()
+        {
+            var meta = new KindMetadata("group", "version", "plural");
+            Assert.Equal("group", meta.Group);
+            Assert.Equal("version", meta.Version);
+            Assert.Equal("plural", meta.Plural);
+        }
+    }
+}
