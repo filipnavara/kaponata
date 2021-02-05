6a90fc2c89db68e121763fe6093b89f5df2d78f3
Bart Saint Germain <bart.saintgermain@quamotion.mobi>
2021-02-05 17:32:32 +0100

    add some logging classes

---
+++ src/Kaponata.Android/Adb/AdbClient.Log.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="AdbClient.Log.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kaponata.Android.Adb
+{
+    /// <summary>
+    /// Contains the <c>ADB</c> logcat methods for the <see cref="AdbClient"/> class.
+    /// </summary>
+    public partial class AdbClient
+    {
+    }
+}
---
+++ src/Kaponata.Android/Adb/Priority.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="Priority.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+namespace Kaponata.Android.Adb
+{
+    /// <summary>
+    /// Represents a log priority.
+    /// </summary>
+    /// <seealso href="https://developer.android.com/reference/android/util/Log.html#ASSERT"/>
+    public enum Priority : byte
+    {
+        /// <summary>
+        /// Represents a verbose message.
+        /// </summary>
+        Verbose = 2,
+
+        /// <summary>
+        /// Represents a debug message.
+        /// </summary>
+        Debug = 3,
+
+        /// <summary>
+        /// Represents an informational message.
+        /// </summary>
+        Info = 4,
+
+        /// <summary>
+        /// Reprents a warning.
+        /// </summary>
+        Warn = 5,
+
+        /// <summary>
+        /// Represents an error.
+        /// </summary>
+        Error = 6,
+
+        /// <summary>
+        /// Represents an assertion which failed.
+        /// </summary>
+        Assert = 7,
+    }
+}
