b8d9cb58df3acc85fdf73d082c67ab8f3e392844
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-02-19 15:47:57 +0100

    Add KubernetesAdbSocketLocator
    Allows creating an AdbClient instance which connects to adb running inside a Kubernetes pod

---
+++ src/Kaponata.Operator.Tests/Kubernetes/KubernetesAdbSocketLocatorTests.cs
@@ -0,0 +1,74 @@
+﻿// <copyright file="KubernetesAdbSocketLocatorTests.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using k8s.Models;
+using Kaponata.Kubernetes;
+using Kaponata.Operator.Kubernetes;
+using Moq;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kaponata.Operator.Tests.Kubernetes
+{
+    /// <summary>
+    /// Tests the <see cref="KubernetesAdbSocketLocator"/> class.
+    /// </summary>
+    public class KubernetesAdbSocketLocatorTests
+    {
+        /// <summary>
+        /// The <see cref="KubernetesAdbSocketLocator"/> constructor validates its arguments.
+        /// </summary>
+        [Fact]
+        public void Constructor_ValidatesArguments()
+        {
+            Assert.Throws<ArgumentNullException>("kubernetes", () => new KubernetesAdbSocketLocator(null, new V1Pod()));
+            Assert.Throws<ArgumentNullException>("pod", () => new KubernetesAdbSocketLocator(Mock.Of<KubernetesClient>(), null));
+        }
+
+        /// <summary>
+        /// <see cref="KubernetesAdbSocketLocator.ConnectToAdbAsync"/> uses Kubernetes port forwarding.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Task"/> which represents the asynchronous test.
+        /// </returns>
+        [Fact]
+        public async Task ConnectToAdbAsync_UsesPortForwarding_Async()
+        {
+            var pod = new V1Pod()
+            {
+                Metadata = new V1ObjectMeta()
+                {
+                    NamespaceProperty = "my-namespace",
+                    Name = "adb-abcd",
+                },
+            };
+
+            var stream = new Mock<Stream>();
+            var kubernetes = new Mock<KubernetesClient>(MockBehavior.Strict);
+            kubernetes
+                .Setup(k => k.ConnectToPodPortAsync(pod, 5037, default))
+                .ReturnsAsync(stream.Object);
+
+            var locator = new KubernetesAdbSocketLocator(kubernetes.Object, pod);
+            using (var value = await locator.ConnectToAdbAsync(default))
+            {
+                Assert.Same(stream.Object, value);
+            }
+
+            kubernetes.Verify();
+        }
+
+        /// <summary>
+        /// <see cref="KubernetesAdbSocketLocator.GetAdbSocket"/> is not implemented.
+        /// </summary>
+        [Fact]
+        public void GetAdbSocket_Throws()
+        {
+            var locator = new KubernetesAdbSocketLocator(Mock.Of<KubernetesClient>(), new V1Pod());
+            Assert.Throws<NotSupportedException>(() => locator.GetAdbSocket());
+        }
+    }
+}
---
+++ src/Kaponata.Operator/Kubernetes/KubernetesAdbSocketLocator.cs
@@ -0,0 +1,55 @@
+﻿// <copyright file="KubernetesAdbSocketLocator.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using k8s.Models;
+using Kaponata.Android.Adb;
+using Kaponata.Kubernetes;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kaponata.Operator.Kubernetes
+{
+    /// <summary>
+    /// An implementation of the <see cref="AdbSocketLocator"/> which connects to adb running in a Kubernetes pod,
+    /// using port forwarding over the API server.
+    /// </summary>
+    public class KubernetesAdbSocketLocator : AdbSocketLocator
+    {
+        private readonly KubernetesClient kubernetes;
+        private readonly V1Pod pod;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KubernetesAdbSocketLocator"/> class.
+        /// </summary>
+        /// <param name="kubernetes">
+        /// A connection to the Kubernetes cluster.
+        /// </param>
+        /// <param name="pod">
+        /// The pod in which adb is running.
+        /// </param>
+        public KubernetesAdbSocketLocator(
+            KubernetesClient kubernetes,
+            V1Pod pod)
+        {
+            this.kubernetes = kubernetes ?? throw new ArgumentNullException(nameof(kubernetes));
+            this.pod = pod ?? throw new ArgumentNullException(nameof(pod));
+        }
+
+        /// <inheritdoc/>
+        public override Task<Stream> ConnectToAdbAsync(CancellationToken cancellationToken)
+        {
+            return this.kubernetes.ConnectToPodPortAsync(this.pod, DefaultAdbPort, cancellationToken).AsTask();
+        }
+
+        /// <inheritdoc/>
+        public override (Socket, EndPoint) GetAdbSocket()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
