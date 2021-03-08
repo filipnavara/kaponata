a0da58e200257e94cec119d0c63468b4bba61faa
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-03-08 16:53:53 +0100

    Kaponata API: Add a Guacamole controller which implements the REST auth provider protocol.

---
+++ src/Kaponata.Api/Guacamole/AuthorizationRequest.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="AuthorizationRequest.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text.Json.Serialization;
+
+namespace Kaponata.Api.Guacamole
+{
+    /// <summary>
+    /// Represents a request to authorize a subject.
+    /// </summary>
+    public class AuthorizationRequest
+    {
+        /// <summary>
+        /// Gets or sets the user name of the subject.
+        /// </summary>
+        [JsonPropertyName("username")]
+        public string? UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password for the user, if any.
+        /// </summary>
+        [JsonPropertyName("password")]
+        public string? Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the network address at which the subject is located, if available.
+        /// </summary>
+        [JsonPropertyName("remoteAddress")]
+        public string? RemoteAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the network host at which the subject is located, if available.
+        /// </summary>
+        [JsonPropertyName("remoteHostname")]
+        public string? RemoteHostName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of HTTP headers associated with the request, and their values.
+        /// </summary>
+        /// <remarks>
+        /// Because a given HTTP header may have multiple values, the object associated with a header name is an array
+        /// of strings, even if there is just one value for a given header name.
+        /// </remarks>
+        [JsonPropertyName("headers")]
+        public Dictionary<string, Collection<string>>? Headers { get; set; }
+    }
+}
---
+++ src/Kaponata.Api/GuacamoleController.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="GuacamoleController.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Kaponata.Api.Guacamole;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kaponata.Api
+{
+    /// <summary>
+    /// Implements an REST API which allows Guacamole to authenticate users using the guacamole-auth-rest extension.
+    /// </summary>
+    [ApiController]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None)]
+    public class GuacamoleController
+    {
+        private readonly ILogger<GuacamoleController> logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GuacamoleController"/> class.
+        /// </summary>
+        /// <param name="logger">
+        /// A logger which is used when logging.
+        /// </param>
+        public GuacamoleController(ILogger<GuacamoleController> logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        /// <summary>
+        /// Processes an authorization request.
+        /// </summary>
+        /// <param name="body">
+        /// The authorization request.
+        /// </param>
+        /// <returns>
+        /// A <see cref="AuthorizationResult"/> which contains the result of the authentication operation.
+        /// </returns>
+        [HttpPost("/api/guacamole/authorization")]
+        public ActionResult Authorize([FromBody] AuthorizationRequest body)
+        {
+            var result = new AuthorizationResult
+            {
+                Authorized = true,
+                Configurations = new Dictionary<string, Configuration>(),
+            };
+
+            return new OkObjectResult(result);
+        }
+    }
+}
