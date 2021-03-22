a7e3a7476210c9074fff99b35c930cf78c84b7cc
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-03-22 11:29:19 +0100

    Add support for reading iOS identities
    Read identity data from X.509 certificates and store them in a POCO

--- src/Kaponata.iOS.Tests/GlobalSuppressions.cs
+++ src/Kaponata.iOS.Tests/GlobalSuppressions.cs
@@ -5,3 +5,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element should begin with upper-case letter", Justification = "Standard iOS spelling", Scope = "namespace", Target = "~N:Kaponata.iOS.Tests.Muxer")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element should begin with upper-case letter", Justification = "Standard iOS spelling", Scope = "namespace", Target = "~N:Kaponata.iOS.Tests.DeveloperProfiles")]
