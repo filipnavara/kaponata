0c1b62b135173d84cd292016ef367fe73bc9902c
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-03-22 16:37:13 +0100

    Support converting SignedCms objects to Kubernetes serets
    iOS Provisioning Profiles are CMS Signed Data objects; support converting them to Kubernetes secrets so they can be stored in the cluster.

---
+++ src/Kaponata.Kubernetes.Tests/DeveloperProfiles/V1SecretExtensionsTests.SignedCms.cs
@@ -0,0 +1,128 @@
+﻿// <copyright file="V1SecretExtensionsTests.SignedCms.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using k8s.Models;
+using Kaponata.Kubernetes.DeveloperProfiles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.Pkcs;
+using Xunit;
+
+namespace Kaponata.Kubernetes.Tests.DeveloperProfiles
+{
+    /// <summary>
+    /// Tests the <see cref="SignedCms"/>-related methods in <see cref="V1SecretExtensions"/>.
+    /// </summary>
+    public partial class V1SecretExtensionsTests
+    {
+        /// <summary>
+        /// Returns tests data for the <see cref="AsSignedCms_ValidatesData(Type, V1Secret)"/> method.
+        /// </summary>
+        /// <returns>
+        /// Tests data for the <see cref="AsSignedCms_ValidatesData(Type, V1Secret)"/> method.
+        /// </returns>
+        public static IEnumerable<object[]> AsSignedCms_ValidatesData_TestData()
+        {
+            yield return new object[]
+            {
+                typeof(ArgumentNullException),
+                (V1Secret)null,
+            };
+
+            yield return new object[]
+            {
+                typeof(InvalidDataException),
+                new V1Secret(),
+            };
+
+            yield return new object[]
+            {
+                typeof(InvalidDataException),
+                new V1Secret()
+                {
+                    Type = "abc",
+                },
+            };
+
+            yield return new object[]
+            {
+                typeof(InvalidDataException),
+                new V1Secret()
+                {
+                    Type = "kaponata.io/signedData",
+                },
+            };
+
+            yield return new object[]
+            {
+                typeof(InvalidDataException),
+                new V1Secret()
+                {
+                    Type = "kubernetes.io/tls",
+                    Data = new Dictionary<string, byte[]>(),
+                },
+            };
+        }
+
+        /// <summary>
+        /// <see cref="V1SecretExtensions.AsSignedCms(V1Secret)"/> throws an exception when passed invalid data.
+        /// </summary>
+        /// <param name="expectedException">
+        /// The expected exception.
+        /// </param>
+        /// <param name="secret">
+        /// The malformed secret.
+        /// </param>
+        [Theory]
+        [MemberData(nameof(AsX509Certificate2_ValidatesData_TestData))]
+        public void AsSignedCms_ValidatesData(Type expectedException, V1Secret secret)
+        {
+            Assert.Throws(expectedException, () => secret.AsX509Certificate2());
+        }
+
+        /// <summary>
+        /// <see cref="V1SecretExtensions.AsSignedCms(V1Secret)"/> returns a valid <see cref="SignedCms"/> object.
+        /// </summary>
+        [Fact]
+        public void AsSignedCms_Works()
+        {
+            // https://github.com/kubernetes-sigs/cluster-api/blob/master/docs/book/src/tasks/certs/using-custom-certificates.md
+            V1Secret secret = new V1Secret()
+            {
+                Data = new Dictionary<string, byte[]>(),
+                Type = "kaponata.io/signedData",
+            };
+
+            secret.Data["signedData"] = File.ReadAllBytes("DeveloperProfiles/test.mobileprovision");
+
+            var cms = secret.AsSignedCms();
+
+            var certificate = cms.Certificates[0];
+            Assert.NotNull(certificate);
+            Assert.Equal("CN=Apple iPhone Certification Authority, OU=Apple Certification Authority, O=Apple Inc., C=US", certificate.Subject);
+            Assert.False(certificate.HasPrivateKey);
+        }
+
+        /// <summary>
+        /// <see cref="V1SecretExtensions.AsSecret(SignedCms)"/> works correctly.
+        /// </summary>
+        [Fact]
+        public void SignedDataAsSecret_Works()
+        {
+            var signedData = new SignedCms();
+            signedData.Decode(File.ReadAllBytes("DeveloperProfiles/test.mobileprovision"));
+
+            var secret = signedData.AsSecret();
+
+            Assert.Equal("v1", secret.ApiVersion);
+            Assert.Equal("Secret", secret.Kind);
+            Assert.Equal("kaponata.io/signedData", secret.Type);
+            Assert.NotNull(secret.Metadata);
+            Assert.NotNull(secret.Metadata.Labels);
+            Assert.Empty(secret.Metadata.Labels);
+            Assert.Equal(File.ReadAllBytes("DeveloperProfiles/test.mobileprovision"), secret.Data["signedData"]);
+        }
+    }
+}
---
+++ src/Kaponata.Kubernetes/DeveloperProfiles/V1SecretExtensions.SignedCms.cs
@@ -0,0 +1,82 @@
+﻿// <copyright file="V1SecretExtensions.SignedCms.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using k8s.Models;
+using Kaponata.iOS.DeveloperProfiles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.Pkcs;
+
+namespace Kaponata.Kubernetes.DeveloperProfiles
+{
+    /// <summary>
+    /// Provides methods for converting <see cref="SignedCms"/> objects to <see cref="V1Secret"/> objects and vice versa.
+    /// </summary>
+    public static partial class V1SecretExtensions
+    {
+        private const string SignedDataContent = "signedData";
+        private const string SignedDataType = "kaponata.io/signedData";
+
+        /// <summary>
+        /// Converts this <see cref="V1Secret"/> to a <see cref="SignedCms"/>.
+        /// </summary>
+        /// <param name="secret">
+        /// A <see cref="V1Secret"/> which represents CMS signed data.
+        /// </param>
+        /// <returns>
+        /// The equivalent <see cref="SignedCms"/> value.
+        /// </returns>
+        public static SignedCms AsSignedCms(this V1Secret secret)
+        {
+            EnsureProvisioningProfileSecret(secret);
+
+            SignedCms cms = new SignedCms();
+            cms.Decode(secret.Data[SignedDataContent]);
+            return cms;
+        }
+
+        /// <summary>
+        /// Converts this <see cref="SignedCms"/> to a <see cref="V1Secret"/>.
+        /// </summary>
+        /// <param name="signedData">
+        /// A <see cref="SignedCms"/> which represents a CMS signed data object.
+        /// </param>
+        /// <returns>
+        /// The equivalent <see cref="V1Secret"/>.
+        /// </returns>
+        public static V1Secret AsSecret(this SignedCms signedData)
+        {
+            var secret = new V1Secret()
+            {
+                ApiVersion = V1Secret.KubeApiVersion,
+                Kind = V1Secret.KubeKind,
+                Type = SignedDataType,
+                Metadata = new V1ObjectMeta()
+                {
+                    Labels = new Dictionary<string, string>(),
+                },
+                Data = new Dictionary<string, byte[]>(),
+                Immutable = true,
+            };
+
+            secret.Data[SignedDataContent] = signedData.Encode();
+
+            return secret;
+        }
+
+        private static void EnsureProvisioningProfileSecret(V1Secret secret)
+        {
+            if (secret == null)
+            {
+                throw new ArgumentNullException(nameof(secret));
+            }
+
+            if (secret.Type != SignedDataType || secret.Data == null || !secret.Data.ContainsKey(SignedDataContent))
+            {
+                throw new InvalidDataException();
+            }
+        }
+    }
+}
