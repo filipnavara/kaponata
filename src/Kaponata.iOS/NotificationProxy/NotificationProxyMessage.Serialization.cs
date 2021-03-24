61a42bc82842f21f48395ff98e78fc1ac43c9e62
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-03-24 22:39:57 +0100

    Add NotificationProxyClient
    Supports observing device notifications.

---
+++ src/Kaponata.iOS/NotificationProxy/NotificationProxyMessage.Serialization.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="NotificationProxyMessage.Serialization.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Claunia.PropertyList;
+using System;
+
+namespace Kaponata.iOS.NotificationProxy
+{
+    /// <summary>
+    /// Provides serialization methods.
+    /// </summary>
+    public partial class NotificationProxyMessage
+    {
+        /// <summary>
+        /// Reads a <see cref="NotificationProxyMessage"/> from a <see cref="NSDictionary"/>.
+        /// </summary>
+        /// <param name="data">
+        /// The message data.
+        /// </param>
+        /// <returns>
+        /// A <see cref="NotificationProxyMessage"/> object.
+        /// </returns>
+        public static NotificationProxyMessage Read(NSDictionary data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            return new NotificationProxyMessage()
+            {
+                Command = data.GetString(nameof(Command)),
+                Name = data.GetString(nameof(Name)),
+            };
+        }
+    }
+}
---
+++ src/Kaponata.iOS/NotificationProxy/NotificationProxyMessage.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="NotificationProxyMessage.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Claunia.PropertyList;
+
+namespace Kaponata.iOS.NotificationProxy
+{
+    /// <summary>
+    /// Represents a message which is sent by or received from the notification proxy running on the device.
+    /// </summary>
+    public partial class NotificationProxyMessage
+    {
+        /// <summary>
+        /// Gets or sets the name of the command.
+        /// </summary>
+        public string Command { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the notification message.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Serializes this <see cref="NotificationProxyMessage"/> to a <see cref="NSDictionary"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="NSDictionary"/> which represents this <see cref="NotificationProxyMessage"/>.
+        /// </returns>
+        public NSDictionary ToPropertyList()
+        {
+            NSDictionary dict = new NSDictionary();
+            dict.Add(nameof(this.Command), this.Command);
+            dict.Add(nameof(this.Name), this.Name);
+            return dict;
+        }
+    }
+}
