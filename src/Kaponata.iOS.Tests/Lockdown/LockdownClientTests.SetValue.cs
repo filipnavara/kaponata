8cabfc750bb1df1df467b23bfab14c66802aa0e1
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-04-13 15:24:32 +0200

    LockdownClient: Implement SetValue

---
+++ src/Kaponata.iOS.Tests/Lockdown/LockdownClientTests.SetValue.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="LockdownClientTests.SetValue.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Claunia.PropertyList;
+using Kaponata.iOS.Lockdown;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kaponata.iOS.Tests.Lockdown
+{
+    /// <content>
+    /// Tests for the <see cref="LockdownClient.SetValueAsync(string, string, string, CancellationToken)"/> family of methods.
+    /// </content>
+    public partial class LockdownClientTests
+    {
+        /// <summary>
+        /// <see cref="LockdownClient.SetValueAsync(string, string, string, CancellationToken)"/> works correctly.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task SetValue_Works_Async()
+        {
+            var dict = new NSDictionary();
+            dict.Add("Request", "GetValue");
+            dict.Add("Key", "my-key");
+            dict.Add("Value", "my-value");
+
+            var protocol = new Mock<LockdownProtocol>(MockBehavior.Strict);
+            protocol
+                .Setup(p => p.WriteMessageAsync(It.IsAny<LockdownMessage>(), default))
+                .Callback<LockdownMessage, CancellationToken>(
+                (message, cancellationToken) =>
+                {
+                    var setValueRequest = Assert.IsType<SetValueRequest>(message);
+                    Assert.Equal("my-domain", setValueRequest.Domain);
+                    Assert.Equal("my-key", setValueRequest.Key);
+                    Assert.Equal("my-value", setValueRequest.Value);
+                })
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+
+            protocol
+                .Setup(p => p.ReadMessageAsync(default))
+                .ReturnsAsync(dict)
+                .Verifiable();
+
+            await using (var client = new LockdownClient(protocol.Object, NullLogger<LockdownClient>.Instance))
+            {
+                await client.SetValueAsync(domain: "my-domain", key: "my-key", value: "my-value", default).ConfigureAwait(false);
+            }
+
+            protocol.Verify();
+        }
+    }
+}
---
+++ src/Kaponata.iOS/Lockdown/LockdownClient.SetValue.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="LockdownClient.SetValue.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kaponata.iOS.Lockdown
+{
+    /// <content>
+    /// GetValue-related methods.
+    /// </content>
+    public partial class LockdownClient
+    {
+        /// <summary>
+        /// Issues a SetValue request.
+        /// </summary>
+        /// <param name="domain">
+        /// The domain of the key to set.
+        /// </param>
+        /// <param name="key">
+        /// The key for the value to set.
+        /// </param>
+        /// <param name="value">
+        /// The value to set the key to.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// A <see cref="CancellationToken"/> which can be used to cancel the asynchronous operation.
+        /// </param>
+        /// <returns>
+        /// The requested value.
+        /// </returns>
+        public virtual async Task SetValueAsync(string domain, string key, string value, CancellationToken cancellationToken)
+        {
+            await this.protocol.WriteMessageAsync(
+                new SetValueRequest()
+                {
+                    Label = this.Label,
+                    Domain = domain,
+                    Key = key,
+                    Value = value,
+                },
+                cancellationToken).ConfigureAwait(false);
+
+            var response = await this.protocol.ReadMessageAsync(cancellationToken).ConfigureAwait(false);
+            var message = LockdownResponse<string>.Read(response);
+
+            this.EnsureSuccess(message);
+        }
+    }
+}
