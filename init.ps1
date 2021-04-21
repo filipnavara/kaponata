a956b3ab353418ce1ee0a9d4ecb2d413c49748d1
Bart Saint Germain <bart.saintgermain@quamotion.mobi>
2021-04-21 15:17:09 +0200

    add nbgv install command

--- init.ps1
+++ init.ps1
@@ -1,13 +1,3 @@
-choco install -y gawk
-choco install -y docker-cli
-choco install -y gnuwin32-coreutils.portable
-choco install -y gomplate
-choco install -y kubernetes-helm
-choco install -y k3d
-choco install -y yq
-choco install -y make
-choco install -y findutils
-
 $env:PATH="$PSScriptRoot\tools;${env:PATH}"
 $env:KUBECONFIG="$PSScriptRoot\ci\kaponata\k3s.yaml"
 $env:DOCKER_HOST="tcp://127.0.0.1:2375"
---
+++ installTools.ps1
@@ -0,0 +1,10 @@
+choco install -y gawk
+choco install -y docker-cli
+choco install -y gnuwin32-coreutils.portable
+choco install -y gomplate
+choco install -y kubernetes-helm
+choco install -y k3d
+choco install -y yq
+choco install -y make
+choco install -y findutils
+dotnet tool install --global nbgv
