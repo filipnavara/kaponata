4b13fab895ed1259d100580909382a8f6eb768e4
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-04-26 14:22:16 +0200

    Add xar file support

---
+++ src/Kaponata.FileFormats.Tests/Xar/XarFileEntryTests.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="XarFileEntryTests.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Kaponata.FileFormats.Xar;
+using System;
+using Xunit;
+
+namespace Kaponata.FileFormats.Tests.Xar
+{
+    /// <summary>
+    /// Tests the <see cref="XarFileEntry"/> class.
+    /// </summary>
+    public class XarFileEntryTests
+    {
+        /// <summary>
+        /// The <see cref="XarFileEntry"/> constructor validates its arguments.
+        /// </summary>
+        [Fact]
+        public void Constructor_ValidatesArguments()
+        {
+            Assert.Throws<ArgumentNullException>(() => new XarFileEntry(null));
+        }
+    }
+}
