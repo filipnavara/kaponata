7dfa2964b0fa6dd69111285c804a6d8a97481423
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-04-26 14:22:35 +0200

    Add CPIO support

---
+++ src/Kaponata.FileFormats/Cpio/CpioFile.cs
@@ -0,0 +1,139 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CpioFile.cs" company="Quamotion">
+//     Copyright (c) 2016 Quamotion bv. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Nerdbank.Streams;
+using System;
+using System.Buffers;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kaponata.FileFormats.Cpio
+{
+    /// <summary>
+    /// The cpio archive format collects any number of files, directories, and
+    /// other file system objects(symbolic links, device nodes, etc.) into a
+    /// single stream of bytes.
+    /// </summary>
+    /// <remarks>
+    /// Each file system object in a cpio archive comprises a header record with
+    /// basic numeric metadata followed by the full pathname of the entry and the
+    /// file data.The header record stores a series of integer values that generally
+    /// follow the fields in struct stat.  (See stat(2) for details.)  The
+    /// variants differ primarily in how they store those integers(binary,
+    /// octal, or hexadecimal).  The header is followed by the pathname of the
+    /// entry(the length of the pathname is stored in the header) and any file
+    /// data.The end of the archive is indicated by a special record with the
+    /// pathname <c>TRAILER!!!</c>.
+    /// </remarks>
+    /// <seealso href="https://people.freebsd.org/~kientzle/libarchive/man/cpio.5.txt"/>
+    public class CpioFile : IDisposable
+    {
+        /// <summary>
+        /// The <see cref="Stream"/> around which this <see cref="CpioFile"/> wraps.
+        /// </summary>
+        private readonly Stream stream;
+
+        /// <summary>
+        /// Indicates whether <see cref="stream"/> should be disposed of when this <see cref="CpioFile"/> is disposed of.
+        /// </summary>
+        private readonly bool leaveOpen;
+
+        private long nextHeaderOffset = 0;
+        private Stream? childStream;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CpioFile"/> class.
+        /// </summary>
+        /// <param name="stream">
+        /// A <see cref="Stream"/> which represents the CPIO data.
+        /// </param>
+        /// <param name="leaveOpen">
+        /// <see langword="true"/> to leave the underlying <paramref name="stream"/> open when this <see cref="CpioFile"/>
+        /// is disposed of; otherwise, <see langword="false"/>.
+        /// </param>
+        public CpioFile(Stream stream, bool leaveOpen)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            this.stream = stream;
+            this.leaveOpen = leaveOpen;
+        }
+
+        /// <summary>
+        /// Reads the next entry in the <see cref="CpioFile"/>.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// A <see cref="CancellationToken"/> which can be used to cancel the asynchronous operation.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if more data is available; otherwise, <see langword="false"/>.
+        /// </returns>
+        public async Task<(CpioHeader? header, string? name, Stream? childStream)> ReadAsync(CancellationToken cancellationToken)
+        {
+            if (this.childStream != null)
+            {
+                await this.childStream.DisposeAsync();
+            }
+
+            if (this.stream.CanSeek)
+            {
+                this.stream.Seek(this.nextHeaderOffset, SeekOrigin.Begin);
+            }
+
+            CpioHeader header = default;
+            string? name = null;
+            this.childStream = null;
+
+            using (var headerBuffer = MemoryPool<byte>.Shared.Rent(CpioHeader.Size))
+            {
+                await this.stream.ReadBlockOrThrowAsync(headerBuffer.Memory.Slice(0, CpioHeader.Size), cancellationToken).ConfigureAwait(false);
+                header.ReadFrom(headerBuffer.Memory.Span);
+
+                if (header.Signature != 0x71C7 /* 070707 in octal */)
+                {
+                    throw new InvalidDataException("The magic for the file entry is invalid");
+                }
+
+                using (var nameBuffer = MemoryPool<byte>.Shared.Rent((int)header.Namesize))
+                {
+                    var nameMemory = nameBuffer.Memory.Slice(0, (int)header.Namesize);
+                    await this.stream.ReadBlockAsync(nameMemory, cancellationToken).ConfigureAwait(false);
+                    name = Encoding.UTF8.GetString(nameMemory.Slice(0, nameMemory.Length - 1).Span);
+                }
+
+                this.nextHeaderOffset = this.stream.Position + header.Filesize;
+                this.childStream = this.stream.ReadSlice(header.Filesize);
+            }
+
+            if (name == "TRAILER!!!")
+            {
+                return (null, null, null);
+            }
+            else if (header.Filesize > 0)
+            {
+                return (header, name, this.childStream);
+            }
+            else
+            {
+                return (header, name, null);
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            if (!this.leaveOpen)
+            {
+                this.stream.Dispose();
+            }
+        }
+    }
+}
---
+++ src/Kaponata.FileFormats/Cpio/CpioHeader.cs
@@ -0,0 +1,125 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CpioHeader.cs" company="Quamotion bv">
+//     Copyright (c) 2016 Quamotion bv. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Text;
+
+namespace Kaponata.FileFormats.Cpio
+{
+    /// <summary>
+    /// Represents the header of an individual entry in a CPIO file, in ASCII format.
+    /// </summary>
+    /// <seealso href="https://people.freebsd.org/~kientzle/libarchive/man/cpio.5.txt"/>
+    public struct CpioHeader
+    {
+        /// <summary>
+        /// The integer value octal 070707.  This value can be used to determine
+        /// whether this archive is written with little-endian or big-endian integers,
+        /// or ASCII.
+        /// </summary>
+        public uint Signature;
+
+        /// <summary>
+        /// The device number from the disk.  These are used by
+        /// programs that read cpio archives to determine when two entries
+        /// refer to the same file.Programs that synthesize cpio archives
+        /// should be careful to set these to distinct values for each entry.
+        /// </summary>
+        public uint Dev;
+
+        /// <summary>
+        /// The inode number from the disk.  These are used by
+        /// programs that read cpio archives to determine when two entries
+        /// refer to the same file.Programs that synthesize cpio archives
+        /// should be careful to set these to distinct values for each entry.
+        /// </summary>
+        public uint Ino;
+
+        /// <summary>
+        /// The mode specifies both the regular permissions and the file type.
+        /// </summary>
+        public LinuxFileMode Mode;
+
+        /// <summary>
+        /// The numeric user id of the owner.
+        /// </summary>
+        public uint Uid;
+
+        /// <summary>
+        /// The numeric group id of the owner.
+        /// </summary>
+        public uint Gid;
+
+        /// <summary>
+        /// The number of links to this file. Directories always have a
+        /// value of at least two here. Note that hardlinked files include
+        /// file data with every copy in the archive.
+        /// </summary>
+        public uint Nlink;
+
+        /// <summary>
+        /// For block special and character special entries, this field contains
+        /// the associated device number.For all other entry types,
+        /// it should be set to zero by writers and ignored by readers.
+        /// </summary>
+        public uint Rdev;
+
+        /// <summary>
+        /// Modification time of the file, indicated as the number of seconds
+        /// since the start of the epoch, 00:00:00 UTC January 1, 1970.  The
+        /// four-byte integer is stored with the most-significant 16 bits
+        /// first followed by the least-significant 16 bits.Each of the two
+        /// 16 bit values are stored in machine-native byte order.
+        /// </summary>
+        public DateTimeOffset Mtime;
+
+        /// <summary>
+        /// The number of bytes in the pathname that follows the header.
+        /// This count includes the trailing NUL byte.
+        /// </summary>
+        public uint Namesize;
+
+        /// <summary>
+        /// The size of the file.  Note that this archive format is limited
+        /// to four gigabyte file sizes. See mtime above for a description
+        /// of the storage of four-byte integers.
+        /// </summary>
+        public long Filesize;
+
+        /// <summary>
+        /// Gets the size of this struct.
+        /// </summary>
+        public static int Size => 76;
+
+        /// <summary>
+        /// Reads data from a <see cref="Span{T}"/>.
+        /// </summary>
+        /// <param name="buffer">
+        /// A buffer which contains the header data.
+        /// </param>
+        /// <returns>
+        /// The number of bytes read.
+        /// </returns>
+        public int ReadFrom(Span<byte> buffer)
+        {
+            this.Signature = ReadOctalString(buffer.Slice(0, 6));
+            this.Dev = ReadOctalString(buffer.Slice(6, 6));
+            this.Ino = ReadOctalString(buffer.Slice(12, 6));
+            this.Mode = (LinuxFileMode)ReadOctalString(buffer.Slice(18, 6));
+            this.Uid = ReadOctalString(buffer.Slice(24, 6));
+            this.Gid = ReadOctalString(buffer.Slice(30, 6));
+            this.Nlink = ReadOctalString(buffer.Slice(36, 6));
+            this.Rdev = ReadOctalString(buffer.Slice(42, 6));
+            this.Mtime = DateTimeOffset.FromUnixTimeSeconds(ReadOctalString(buffer.Slice(48, 11)));
+            this.Namesize = ReadOctalString(buffer.Slice(59, 6));
+            this.Filesize = ReadOctalString(buffer.Slice(65, 11));
+
+            return Size;
+        }
+
+        private static uint ReadOctalString(Span<byte> data) => Convert.ToUInt32(Encoding.UTF8.GetString(data), 8);
+    }
+}
