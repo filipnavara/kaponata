42c2566600bc3c584fb3299d21cdd1be34a56db7
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-05-03 22:30:23 +0200

    Point: Add unit tests

---
+++ src/Kaponata.FileFormats.Tests/HfsPlus/PointTests.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="PointTests.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using DiscUtils.HfsPlus;
+using System;
+using Xunit;
+
+namespace Kaponata.FileFormats.Tests.HfsPlus
+{
+    /// <summary>
+    /// Tests the <see cref="Point"/> class.
+    /// </summary>
+    public class PointTests
+    {
+        /// <summary>
+        /// <see cref="Point.ReadFrom(byte[], int)"/> reads the data.
+        /// </summary>
+        [Fact]
+        public void ReadFrom_ReadsData()
+        {
+            var point = new Point();
+            point.ReadFrom(new byte[4], 0);
+            Assert.Equal(0, point.Horizontal);
+            Assert.Equal(0, point.Vertical);
+        }
+
+        /// <summary>
+        /// <see cref="Point.WriteTo(byte[], int)"/> throws.
+        /// </summary>
+        [Fact]
+        public void WriteTo_Throws()
+        {
+            var point = new Point();
+            Assert.Throws<NotImplementedException>(() => point.WriteTo(Array.Empty<byte>(), 0));
+        }
+    }
+}
