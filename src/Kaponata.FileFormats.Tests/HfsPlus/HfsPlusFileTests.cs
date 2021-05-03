f5bc03e738bde4b818eddf26bda4d02dbe928254
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-05-03 17:42:59 +0200

    HfsPlusFileTests: Add coverage for zlib-attribute-compressed data.

--- src/Kaponata.FileFormats.Tests/HfsPlus/HfsPlusFileTests.cs
+++ src/Kaponata.FileFormats.Tests/HfsPlus/HfsPlusFileTests.cs
@@ -47,5 +47,38 @@ namespace Kaponata.FileFormats.Tests.HfsPlus
             Assert.Equal(8, file.FileContent.Read(0, buffer, 0, buffer.Length));
             Assert.Equal("APPL????", Encoding.UTF8.GetString(buffer, 0, 8));
         }
+
+        /// <summary>
+        /// Tests the <see cref="HfsPlusFile.FileContent"/> property in a scenario where the content is marked as <see cref="FileCompressionType.ZlibAttribute"/>,
+        /// at the attribute level, and zlib-compressed.
+        /// </summary>
+        [Fact]
+        public void FileContent_ZlibAttribute_Compressed()
+        {
+            var nodeId = new CatalogNodeId(1);
+            byte[] compressionAttribute = Convert.FromBase64String("AAAAEAAAAAAAAAAAAAAALWZwbWMDAAAAKgAAAAAAAAB4XksqyMksLjEwuMDBAAaMjAxQBgMq4AQAjygD0wA=");
+
+            var attributes = new Mock<BTree<AttributeKey>>(MockBehavior.Strict);
+            attributes
+                .Setup(a => a.Find(new AttributeKey(nodeId, "com.apple.decmpfs")))
+                .Returns(compressionAttribute);
+
+            var context = new Context()
+            {
+                Attributes = attributes.Object,
+            };
+
+            var catalogInfo = new CatalogFileInfo()
+            {
+                FileId = nodeId,
+            };
+
+            var file = new HfsPlusFile(context, nodeId, catalogInfo);
+
+            byte[] buffer = new byte[0x40];
+
+            Assert.Equal(42, file.FileContent.Read(0, buffer, 0, buffer.Length));
+            Assert.Equal("bplist00", Encoding.UTF8.GetString(buffer, 0, 8));
+        }
     }
 }
