b9e633a09c54708a5d4e45da6635ad2a104e170b
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-05-04 16:48:13 +0200

    Add the SystemVersion class

---
+++ src/Kaponata.iOS.Tests/DeveloperDisks/SystemVersionTests.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="SystemVersionTests.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Claunia.PropertyList;
+using Kaponata.iOS.DeveloperDisks;
+using System;
+using Xunit;
+
+namespace Kaponata.iOS.Tests.DeveloperDisks
+{
+    /// <summary>
+    /// Tests the <see cref="SystemVersion"/> class.
+    /// </summary>
+    public class SystemVersionTests
+    {
+        /// <summary>
+        /// The <see cref="SystemVersion.FromDictionary(NSDictionary)"/> method throws when passed
+        /// <see langword="null"/> value.
+        /// </summary>
+        [Fact]
+        public void FromDictionary_ThrowsOnNull()
+        {
+            var version = new SystemVersion();
+            Assert.Throws<ArgumentNullException>(() => version.FromDictionary(null));
+        }
+
+        /// <summary>
+        /// <see cref="SystemVersion.FromDictionary(NSDictionary)"/> correctly parses data.
+        /// </summary>
+        [Fact]
+        public void FromDictionary_Works()
+        {
+            // Create a dict with sample data seen in e.g.
+            // https://github.com/WebKit/WebKit/blob/master/Tools/CISupport/ews-build/steps_unittest.py#L3783-L3789
+            var dict = new NSDictionary();
+            dict.Add("BuildID", "BB4C82AE-5F8A-11EA-A1A5-838AD03DDE06");
+            dict.Add("ProductBuildVersion", "17E255");
+            dict.Add("ProductCopyright", "1983-2020 Apple Inc.");
+            dict.Add("ProductName", "iPhone OS");
+            dict.Add("ProductVersion", "13.4");
+
+            var version = new SystemVersion();
+            version.FromDictionary(dict);
+
+            Assert.Equal(new Guid("BB4C82AE-5F8A-11EA-A1A5-838AD03DDE06"), version.BuildID);
+            Assert.Equal(AppleVersion.Parse("17E255"), version.ProductBuildVersion);
+            Assert.Equal("1983-2020 Apple Inc.", version.ProductCopyright);
+            Assert.Equal("iPhone OS", version.ProductName);
+            Assert.Equal(new Version("13.4"), version.ProductVersion);
+        }
+    }
+}
---
+++ src/Kaponata.iOS/DeveloperDisks/SystemVersion.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="SystemVersion.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Claunia.PropertyList;
+using Kaponata.iOS.PropertyLists;
+using System;
+
+namespace Kaponata.iOS.DeveloperDisks
+{
+    /// <summary>
+    /// Represents the version of an operating system or a SDK.
+    /// </summary>
+    public class SystemVersion : IPropertyListDeserializable
+    {
+        /// <summary>
+        /// Gets or sets a <see cref="Guid"/> which uniquely identifies the product build.
+        /// </summary>
+        public Guid BuildID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the product build version.
+        /// </summary>
+        public AppleVersion ProductBuildVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the copyright owner.
+        /// </summary>
+        public string ProductCopyright { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the product.
+        /// </summary>
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version of the product.
+        /// </summary>
+        public Version ProductVersion { get; set; }
+
+        /// <inheritdoc/>
+        public void FromDictionary(NSDictionary dictionary)
+        {
+            if (dictionary == null)
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+
+            this.BuildID = new Guid(dictionary.GetString(nameof(this.BuildID)));
+            this.ProductBuildVersion = AppleVersion.Parse(dictionary.GetString(nameof(this.ProductBuildVersion)));
+            this.ProductCopyright = dictionary.GetString(nameof(this.ProductCopyright));
+            this.ProductName = dictionary.GetString(nameof(this.ProductName));
+            this.ProductVersion = new Version(dictionary.GetString(nameof(this.ProductVersion)));
+        }
+    }
+}
