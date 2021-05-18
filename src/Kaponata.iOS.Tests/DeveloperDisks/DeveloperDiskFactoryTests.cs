83f97c95c7e58d6f0132232f752f14ef987cb2d2
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-05-18 13:11:24 +0200

    DeveloperDisk: Move static methods to a Factory class

---
+++ src/Kaponata.iOS.Tests/DeveloperDisks/DeveloperDiskFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="DeveloperDiskFactoryTests.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Kaponata.iOS.DeveloperDisks;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kaponata.iOS.Tests.DeveloperDisks
+{
+    /// <summary>
+    /// Tests the <see cref="DeveloperDiskFactory"/> class.
+    /// </summary>
+    public class DeveloperDiskFactoryTests
+    {
+        /// <summary>
+        /// <see cref="DeveloperDiskFactory.FromFileAsync(Stream, Stream, CancellationToken)"/> validates its arguments.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task FromFileAsync_ValidatesArguments_Async()
+        {
+            var factory = new DeveloperDiskFactory();
+            await Assert.ThrowsAsync<ArgumentNullException>(() => factory.FromFileAsync(null, Stream.Null, default));
+            await Assert.ThrowsAsync<ArgumentNullException>(() => factory.FromFileAsync(Stream.Null, null, default));
+        }
+
+        /// <summary>
+        /// <see cref="DeveloperDiskFactory.FromFileAsync(Stream, Stream, CancellationToken)"/> works as intended.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task FromFileAsync_Works_Async()
+        {
+            using (Stream stream = File.OpenRead("TestAssets/udro-hfsplus.dmg"))
+            using (MemoryStream signature = new MemoryStream(new byte[] { 1, 2, 3, 4 }))
+            {
+                var factory = new DeveloperDiskFactory();
+                var disk = await factory.FromFileAsync(stream, signature, default).ConfigureAwait(false);
+
+                Assert.Same(stream, disk.Image);
+                Assert.Equal(new byte[] { 1, 2, 3, 4 }, disk.Signature);
+                Assert.NotNull(disk.Version);
+                Assert.Equal("17E255", disk.Version.ProductBuildVersion.ToString());
+            }
+        }
+    }
+}
--- src/Kaponata.iOS/DeveloperDisks/DeveloperDisk.cs
+++ src/Kaponata.iOS/DeveloperDisks/DeveloperDisk.cs
@@ -39,50 +39,6 @@ namespace Kaponata.iOS.DeveloperDisks
         /// </summary>
         public DateTimeOffset CreationTime { get; set; }
 
-        /// <summary>
-        /// Creates a <see cref="DeveloperDisk"/> object based on a file.
-        /// </summary>
-        /// <param name="image">
-        /// A <see cref="Stream"/> which represents the developer disk image.
-        /// </param>
-        /// <param name="signature">
-        /// A <see cref="Stream"/> which represents the developer disk signature.
-        /// </param>
-        /// <param name="cancellationToken">
-        /// A <see cref="CancellationToken"/> which can be used to cancel the asynchronous operation.
-        /// </param>
-        /// <returns>
-        /// A <see cref="Task"/> which represents the asynchronous operation and, when completed,
-        /// returns a <see cref="DeveloperDisk"/> object which represents the developer disk.
-        /// </returns>
-        public static async Task<DeveloperDisk> FromFileAsync(Stream image, Stream signature, CancellationToken cancellationToken)
-        {
-            if (image == null)
-            {
-                throw new ArgumentNullException(nameof(image));
-            }
-
-            if (signature == null)
-            {
-                throw new ArgumentNullException(nameof(signature));
-            }
-
-            (var version, var creationTime) = DeveloperDiskReader.GetVersionInformation(image);
-            image.Seek(0, SeekOrigin.Begin);
-
-            byte[] signatureBytes = new byte[signature.Length];
-            signature.Seek(0, SeekOrigin.Begin);
-            await signature.ReadBlockOrThrowAsync(signatureBytes, cancellationToken).ConfigureAwait(false);
-
-            return new DeveloperDisk()
-            {
-                Image = image,
-                Signature = signatureBytes,
-                Version = version,
-                CreationTime = creationTime,
-            };
-        }
-
         /// <inheritdoc/>
         public void Dispose()
         {
