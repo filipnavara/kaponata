6788095898b68e4e3233cf61e151504010d2ba1f
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-05-21 13:31:24 +0200

    Use a dedicated DeviceServiceScope
    Interacting with lockdown services on iOS devices requires you to create a `IServiceScope`. This `IServiceScope` must have a `DeviceContext` service which contains a at least the `MuxerDevice` object, a `MuxerClient` service and a factory which can create your service.
    
    We previously implemented this using extension methods on the `IServiceScope` interface, but these are not easy to mock.
    
    This PR rewrites the functionality to use a dedicated `DeviceServiceScope` object, from which you can source the required services.

--- src/Kaponata.iOS.Tests/DependencyInjection/DeviceServiceScopeTests.cs
+++ src/Kaponata.iOS.Tests/DependencyInjection/DeviceServiceScopeTests.cs
@@ -4,28 +4,81 @@
 
 using Kaponata.iOS.DependencyInjection;
 using Kaponata.iOS.Lockdown;
-using Kaponata.iOS.Muxer;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Kaponata.iOS.Tests.DependencyInjection
 {
     /// <summary>
-    /// Tests the <see cref="DeviceServiceScope{T}"/> class.
+    /// Tests the <see cref="DeviceServiceScope"/> class.
     /// </summary>
     public class DeviceServiceScopeTests
     {
         /// <summary>
-        /// The <see cref="DeviceServiceScope{T}"/> constructor validates its arguments.
+        /// The <see cref="DeviceServiceScope"/> constructor validates its arguments.
         /// </summary>
         [Fact]
         public void Constructor_ValidatesArguments()
         {
-            Assert.Throws<ArgumentNullException>(() => new DeviceServiceScope<LockdownClient>(null, new MuxerDevice(), Mock.Of<LockdownClient>()));
-            Assert.Throws<ArgumentNullException>(() => new DeviceServiceScope<LockdownClient>(Mock.Of<IServiceScope>(), null, Mock.Of<LockdownClient>()));
-            Assert.Throws<ArgumentNullException>(() => new DeviceServiceScope<LockdownClient>(Mock.Of<IServiceScope>(), new MuxerDevice(), null));
+            Assert.Throws<ArgumentNullException>(() => new DeviceServiceScope(null));
+        }
+
+        /// <summary>
+        /// The <see cref="DeviceServiceScope"/> constructor throws an exception if the service scope
+        /// does not contain at least a <see cref="DeviceContext"/> value.
+        /// </summary>
+        [Fact]
+        public void Constructor_RequiresDeviceContext()
+        {
+            using (var provider = new ServiceCollection().BuildServiceProvider())
+            using (var scope = provider.CreateScope())
+            {
+                Assert.Throws<InvalidOperationException>(() => new DeviceServiceScope(scope));
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="DeviceServiceScope"/> constructor correctly initializes the properties.
+        /// </summary>
+        [Fact]
+        public void Constructor_InitializesProperties()
+        {
+            using (var provider = new ServiceCollection()
+                .AddScoped<DeviceContext>()
+                .BuildServiceProvider())
+            using (var scope = provider.CreateScope())
+            using (var deviceScope = new DeviceServiceScope(scope))
+            {
+                Assert.Equal(scope.ServiceProvider, deviceScope.ServiceProvider);
+                Assert.NotNull(deviceScope.Context);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="DeviceServiceScope.StartServiceAsync{T}(CancellationToken)"/> returns a service client.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task StartServiceAsync_Works_Async()
+        {
+            var lockdown = new Mock<LockdownClient>(MockBehavior.Strict);
+
+            var lockdownFactory = new Mock<ClientFactory<LockdownClient>>(MockBehavior.Strict);
+            lockdownFactory.Setup(l => l.CreateAsync(default)).ReturnsAsync(lockdown.Object);
+
+            using (var provider = new ServiceCollection()
+                .AddScoped<DeviceContext>()
+                .AddScoped<ClientFactory<LockdownClient>>((sp) => lockdownFactory.Object)
+                .BuildServiceProvider())
+            using (var scope = provider.CreateScope())
+            using (var deviceScope = new DeviceServiceScope(scope))
+            {
+                Assert.Equal(lockdown.Object, await deviceScope.StartServiceAsync<LockdownClient>(default).ConfigureAwait(false));
+            }
         }
     }
 }
