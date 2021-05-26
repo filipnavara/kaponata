e83bfc8b141a23c6df8c99e3e134d2190beeb6e4
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-05-26 16:54:41 +0200

    Support device conditions.

---
+++ src/Kaponata.Kubernetes/Models/MobileDeviceCondition.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="MobileDeviceCondition.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+#nullable disable
+
+namespace Kaponata.Kubernetes.Models
+{
+    /// <summary>
+    /// <see cref="MobileDeviceCondition"/> contains details for the current condition of this <see cref="MobileDevice"/>.
+    /// </summary>
+    /// <seealso href="https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties"/>
+    public class MobileDeviceCondition
+    {
+        /// <summary>
+        /// Gets or sets the type of the condition. The condition type uniquely identifies the condition.
+        /// </summary>
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status of the condition.
+        /// </summary>
+        [JsonProperty("status")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ConditionStatus Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets a (brief) reason for the condition's last transition.
+        /// </summary>
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
+
+        /// <summary>
+        /// Gets or sets human readable message indicating details about last transition.
+        /// </summary>
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last time the condition was probed.
+        /// </summary>
+        [JsonProperty("lastHeartbeatTime")]
+        public DateTimeOffset LastHeartbeatTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last time the device condition transitioned from one status to another.
+        /// </summary>
+        [JsonProperty("lastTransitionTime")]
+        public DateTimeOffset LastTransitionTime { get; set; }
+    }
+}
--- src/Kaponata.Kubernetes/Models/MobileDeviceStatus.cs
+++ src/Kaponata.Kubernetes/Models/MobileDeviceStatus.cs
@@ -2,7 +2,11 @@
 // Copyright (c) Quamotion bv. All rights reserved.
 // </copyright>
 
+using Microsoft.AspNetCore.JsonPatch;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 #nullable disable
 
@@ -26,5 +30,71 @@ namespace Kaponata.Kubernetes.Models
         /// </summary>
         [JsonProperty(PropertyName = "vncPassword")]
         public string VncPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of all conditions currently known for this device.
+        /// </summary>
+        [JsonProperty(PropertyName = "conditions")]
+        public IList<MobileDeviceCondition> Conditions { get; set; }
+
+        /// <summary>
+        /// Updates a condition.
+        /// </summary>
+        /// <param name="type">
+        /// The name of the condition to update.
+        /// </param>
+        /// <param name="status">
+        /// The current status of the condition.
+        /// </param>
+        /// <param name="reason">
+        /// A machine-readable status code which describes the reason for the condition.
+        /// </param>
+        /// <param name="message">
+        /// A human-readable message which describes the reason for the condition.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the condition has been updated; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        public bool UpdateCondition(string type, ConditionStatus status, string reason, string message)
+        {
+            var currentCondition = this.Conditions.SingleOrDefault(c => c.Type == type);
+
+            if (currentCondition == null)
+            {
+                this.Conditions.Add(
+                    new MobileDeviceCondition()
+                    {
+                        Type = type,
+                        Status = status,
+                        Reason = reason,
+                        Message = message,
+                        LastHeartbeatTime = DateTimeOffset.Now,
+                        LastTransitionTime = DateTimeOffset.Now,
+                    });
+
+                return true;
+            }
+            else if (currentCondition.Status != status)
+            {
+                currentCondition.LastHeartbeatTime = DateTimeOffset.Now;
+                currentCondition.LastTransitionTime = DateTimeOffset.Now;
+                currentCondition.Status = status;
+                currentCondition.Reason = reason;
+                currentCondition.Message = message;
+
+                return true;
+            }
+            else if (currentCondition.LastHeartbeatTime.AddMinutes(5) > DateTimeOffset.Now)
+            {
+                currentCondition.LastHeartbeatTime = DateTimeOffset.Now;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
