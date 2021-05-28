31a6b0d5aeb62bf85a3ce9428c7c6677c569ea84
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-05-28 14:43:04 +0200

    Add extension methods for updating the status condition of a mobile device in one go.

---
+++ src/Kaponata.Kubernetes/NamespacedKubernetesClientExtensions.MobileDevice.cs
@@ -0,0 +1,63 @@
+﻿// <copyright file="NamespacedKubernetesClientExtensions.MobileDevice.cs" company="Quamotion bv">
+// Copyright (c) Quamotion bv. All rights reserved.
+// </copyright>
+
+using Kaponata.Kubernetes.Models;
+using Microsoft;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kaponata.Kubernetes
+{
+    /// <summary>
+    /// Provides extension methods for the <see cref="NamespacedKubernetesClient{T}"/> class.
+    /// </summary>
+    public static class NamespacedKubernetesClientExtensions
+    {
+        /// <summary>
+        /// Asynchronously sets a device condition.
+        /// </summary>
+        /// <param name="client">
+        /// A client which provides access to the Kubernetes cluster.
+        /// </param>
+        /// <param name="device">
+        /// The device for which to set the status.
+        /// </param>
+        /// <param name="type">
+        /// The name of the condition.
+        /// </param>
+        /// <param name="status">
+        /// The status of the condition.
+        /// </param>
+        /// <param name="reason">
+        /// A machine-readable reason for the condition status.
+        /// </param>
+        /// <param name="message">
+        /// A human-readable reason for the condition status.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// A <see cref="CancellationToken"/> which can be used to cancel the asynchronous operation.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task"/> which represents the asynchronous operation.
+        /// </returns>
+        public static async Task SetDeviceConditionAsync(this NamespacedKubernetesClient<MobileDevice> client, MobileDevice device, string type, ConditionStatus status, string reason, string message, CancellationToken cancellationToken)
+        {
+            Requires.NotNull(device, nameof(device));
+            Requires.NotNull(type, nameof(type));
+
+            if (device.Status == null)
+            {
+                device.Status = new MobileDeviceStatus();
+            }
+
+            if (device.Status.SetCondition(type, status, reason, message))
+            {
+                var patch = new JsonPatchDocument<MobileDevice>();
+                patch.Replace(p => p.Status.Conditions, device.Status.Conditions);
+                await client.PatchStatusAsync(device, patch, cancellationToken).ConfigureAwait(false);
+            }
+        }
+    }
+}
