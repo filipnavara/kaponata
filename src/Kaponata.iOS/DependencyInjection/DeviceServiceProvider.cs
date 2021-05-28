c4e02c1eaad7188f639643ae209e81b15f987c2a
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-05-28 15:36:05 +0200

    DeveloperDiskProvisioner: Operate on MuxerDevice objects instead of udids

--- src/Kaponata.iOS/DependencyInjection/DeviceServiceProvider.cs
+++ src/Kaponata.iOS/DependencyInjection/DeviceServiceProvider.cs
@@ -3,6 +3,7 @@
 // </copyright>
 
 using Kaponata.iOS.Muxer;
+using Microsoft;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
@@ -69,10 +70,31 @@ namespace Kaponata.iOS.DependencyInjection
                 throw new MuxerException($"Could not find the device with udid '{udid}'.");
             }
 
+            return await this.CreateDeviceScopeAsync(devices[0], cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Asynchronously create a device scope, which can be used to interact with services running on the iOS device.
+        /// </summary>
+        /// <param name="device">
+        /// The device to which to connect.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// A <see cref="CancellationToken"/> which can be used to cancel the asynchronous operation.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task"/> which represents the asynchronous operation, and returns a <see cref="IServiceScope"/>
+        /// from which device services (such as lockdown service clients) can be sourced.
+        /// </returns>
+        public virtual async Task<DeviceServiceScope> CreateDeviceScopeAsync(MuxerDevice device, CancellationToken cancellationToken)
+        {
+            Requires.NotNull(device, nameof(device));
+
+            var muxer = this.provider.GetRequiredService<MuxerClient>();
             var scope = this.provider.CreateScope();
 
             var context = scope.ServiceProvider.GetRequiredService<DeviceContext>();
-            context.Device = devices[0];
+            context.Device = device;
 
             context.PairingRecord = await muxer.ReadPairingRecordAsync(context.Device.Udid, cancellationToken).ConfigureAwait(false);
 
