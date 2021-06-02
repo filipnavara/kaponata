bc7363503e69f6645c48231215198ad467f65a55
Bart Saint Germain <bart.saintgermain@quamotion.mobi>
2021-06-02 15:16:35 +0200

    adding FontAwesomeModule

--- src/kaponata-ui/src/app/app.module.ts
+++ src/kaponata-ui/src/app/app.module.ts
@@ -8,22 +8,34 @@ import { AppRoutingModule} from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { ProvisioningProfileService } from './provisioning-profile.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
     AppComponent,
     SettingsComponent,
     PageNotFoundComponent,
+    FileUploadComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     NgbModule,
+    FontAwesomeModule,
   ],
   providers: [
     ProvisioningProfileService
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule
+{
+  constructor(library: FaIconLibrary) {
+      // Add an icon to the library for convenient access in other components
+      library.addIcons(faTrash);
+  }
+}
