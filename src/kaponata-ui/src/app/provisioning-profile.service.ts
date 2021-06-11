6f57fb80bc300015bae1f3b90e8f3e85cc391c90
Bart Saint Germain <bart.saintgermain@quamotion.mobi>
2021-06-11 17:24:14 +0200

    intecept errors and add placeholder for errors

--- src/kaponata-ui/src/app/provisioning-profile.service.ts
+++ src/kaponata-ui/src/app/provisioning-profile.service.ts
@@ -11,25 +11,6 @@ export class ProvisioningProfileService {
   constructor(public http: HttpClient) { }
 
   getProvisioningProfiles(): Observable<ProvisioningProfile[]> {
-    return this.http.get<ProvisioningProfile[]>('http://localhost/api/ios/provisioningProfiles')
-      .pipe(
-        catchError(this.handleError)
-      );
-  }
-
-  private handleError(error: HttpErrorResponse): Observable<never> {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(
-      'Something bad happened; please try again later.');
+    return this.http.get<ProvisioningProfile[]>('http://localhost/api/ios/provisioningProfiles');
   }
 }
